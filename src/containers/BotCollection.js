923ef17d17c60812090293e7e19dca02699d1d08
github username <github email address>
2019-09-05 11:08:24 -0500

    nailed it

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,20 +1,33 @@
 import React from "react";
 import BotCard from "../components/BotCard";
 
+
 class BotCollection extends React.Component {
-  //your code here
+	constructor(props) {
+		super(props);
+		const { bots } = props
+		this.state = {bots}
+		}
+	
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
-  }
+	render() {
+		const oneBot = this.state.bots.map(bot => <oneBotty key={bot.id} name={bot.name} />
 
-};
+		return (
+		  <div className="ui four column grid">
+			  <div className="row">
+				Collection of all bots
+				<BotCard bot={oneBot}/>
+			  </div>
+		  </div>
+		);
+	}
+  };
+
+
+  // I am trying to map over the collection of bots and pass in a single bot as props with its attributes to the botCard. 
+
 
 export default BotCollection;
+
+
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,11 +1,37 @@
 import React from "react";
+import BotCollection from './BotCollection'
+// import BotCard from './components/BotCard'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+	  super ()
+    this.state = {
+      botsData: 'bots'}
+  }
+
+
+
+componentDidMount(){
+	this.fetchBots();
+}
+
+  fetchBots() {
+	  const botURL = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
+	  fetch(botURL)
+	  .then(resp => resp.json())
+	  .then(bots => {
+      this.setState({botsData: bots})
+	  })
+	  .catch(err => console.log(err))
+  }
 
   render() {
+
     return (
       <div>
+          <BotCollection bots={this.state.botsData}/>
+          
         {/* put your components here */}
       </div>
     );
@@ -14,3 +40,4 @@ class BotsPage extends React.Component {
 }
 
 export default BotsPage;
+
