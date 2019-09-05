4dcdd1d32f8cf573befbd9e25350c0c8342268c6
MattOrvin <matthew.orvin@gmail.com>
2019-09-05 11:06:33 -0500

    done

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,32 @@
+// This is the main container for the page
+// This is the parent of BotCollection
+
 import React from "react";
+import BotCollection from './BotCollection';
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  // constructor(props){
+	//   super(props)
+	//   // this.state={props: }
+  // }
+  
   render() {
     return (
       <div>
-        {/* put your components here */}
+        {/* put your components here */
+          <BotCollection />
+        }
       </div>
     );
   }
-
+  
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(resp => resp.json())
+      .then(botData => this.setState({botData}))
+  }
 }
 
 export default BotsPage;
